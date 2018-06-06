52d60997ca349253cb907b48087091a2d65baed0
Mikolla <miqolay@gmail.com>
2018-06-06 10:27:43 +0500

    3.2. Конвертер валюты. исправление checkstyle

--- chapter_001/src/main/java/ru/job4j/converter/Converter.java
+++ chapter_001/src/main/java/ru/job4j/converter/Converter.java
@@ -11,7 +11,7 @@ public class Converter {
      * @return Евро.
      */
     public int rubleToEuro(int value) {
-        return value/70;
+        return value / 70;
     }
 
     /**
@@ -20,7 +20,7 @@ public class Converter {
      * @return Доллары
      */
     public int rubleToDollar(int value) {
-        return value/60;
+        return value / 60;
     }
 
 
@@ -30,7 +30,7 @@ public class Converter {
      * @return Евро.
      */
     public int euroToRuble(int value) {
-        return value*70;
+        return value * 70;
     }
 
     /**
@@ -39,7 +39,7 @@ public class Converter {
      * @return Доллары
      */
     public int dollarToRuble(int value) {
-        return value*60;
+        return value * 60;
     }
 
 }
