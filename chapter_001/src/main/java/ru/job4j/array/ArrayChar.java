c7a98556c42d0417cb2255872d79c94b1c4dbd87
Mikolla <miqolay@gmail.com>
2018-06-14 13:59:54 +0500

    6.4. Слова начинается с ... [#41585]

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -20,14 +20,18 @@ public class ArrayChar {
      * @return если слово начинаеться с префикса
      */
     public boolean startWith(String prefix) {
+           boolean result = true;
            char[] value = prefix.toCharArray();
         // проверить. что массив data имеет первые элементы одинаковые с value
         for (int i = 0; i < value.length; i++) {
             if (value[i] != data[i]) {
-                return false;
+                result = false;
             }
         }
-
-        return true;
+        return result;
     }
+
+
+
+
 }
