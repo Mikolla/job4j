f797d346b5bf904aee6ba9333776e46938385c22
Mikolla <miqolay@gmail.com>
2018-06-27 01:17:00 +0600

    Крестики-нолики на JavaFX

--- chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -39,35 +39,17 @@ public class Logic3T {
         }
         return this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1)
             || this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, -1, 1);
-
-        /*  return this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0)
-                || this.fillBy(Figure3T::hasMarkX, 0, 1, 1, 0)
-                || this.fillBy(Figure3T::hasMarkX, 0, 2, 1, 0)
-                || this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1)
-                || this.fillBy(Figure3T::hasMarkX, 1, 0, 0, 1)
-                || this.fillBy(Figure3T::hasMarkX, 2, 0, 0, 1)
-                || this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1)
-                || this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, -1, 1); */
     }
 
     public boolean isWinnerO() {
         for (int i = 0; i < this.table.length; i++) {
             if (this.fillBy(Figure3T::hasMarkO, 0, i, 1, 0)
-                    || this.fillBy(Figure3T::hasMarkO, i, 0, 0, 1)) {
+             || this.fillBy(Figure3T::hasMarkO, i, 0, 0, 1)) {
                 return true;
             }
         }
         return this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 1)
-                || this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1);
-
-          /*  return this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 0)
-                || this.fillBy(Figure3T::hasMarkO, 0, 1, 1, 0)
-                || this.fillBy(Figure3T::hasMarkO, 0, 2, 1, 0)
-                || this.fillBy(Figure3T::hasMarkO, 0, 0, 0, 1)
-                || this.fillBy(Figure3T::hasMarkO, 1, 0, 0, 1)
-                || this.fillBy(Figure3T::hasMarkO, 2, 0, 0, 1)
-                || this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 1)
-                || this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1); */
+            || this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1);
     }
 
     public boolean hasGap() {
