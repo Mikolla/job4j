ddccfabd228624775e9798f2112ae3b2a4189c11
Mikolla <miqolay@gmail.com>
2018-07-02 16:21:47 +0500

    1. Реализация профессий в коде [#6837]

--- chapter_002/src/main/java/ru/job4j/profession/Profession.java
+++ chapter_002/src/main/java/ru/job4j/profession/Profession.java
@@ -1,4 +1,24 @@
 package ru.job4j.profession;
 
 public class Profession {
+
+    String name;
+    String profession;
+
+    public Profession() {
+
+    }
+
+    public Profession(String name, String profession) {
+        this.name = name;
+        this.profession = profession;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+
+
+
 }
