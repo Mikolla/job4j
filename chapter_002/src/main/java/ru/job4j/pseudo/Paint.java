8e0a74c9666a64bd5c56c4e71ff61510be34972d
Mikolla <miqolay@gmail.com>
2018-07-23 08:26:18 +0600

    4. Используя шаблон проектирования - стратегию

---
+++ chapter_002/src/main/java/ru/job4j/pseudo/Paint.java
@@ -0,0 +1,15 @@
+//c:\projects\job4j>java -classpath C:\projects\job4j\chapter_002\target\classes ru.job4j.pseudo.Paint
+
+package ru.job4j.pseudo;
+
+
+public class Paint {
+    public void draw(Shape shape) {
+         System.out.println(shape.draw());  
+    }
+	
+	public static void main(String[] args) {
+	    new Paint().draw(new Square());
+    }
+
+}
